a0c44d17ef0176de69c02e8a44fc9e47fd286acb
tami <tamiortami@gmail.com>
2025-03-09 21:46:24 +0100

    test: i18n

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
+import { useTranslation } from "react-i18next";
 
 function Home() {
-
+  const { t } = useTranslation();
   return (
     <>
         <div>
-    Home
+   <h1>{t('Welcome to React')}</h1>
     </div>
     </>
   )

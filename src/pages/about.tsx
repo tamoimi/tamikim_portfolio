8844631d97ac4834f05aa1bb9686dcdfd7cc990a
tami <tamiortami@gmail.com>
2025-03-10 17:00:40 +0100

    feat: text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,56 @@
-
 function About() {
-
   return (
     <>
-        <div>
-    ABOUT
-    </div>
+      <div className="flex flex-col gap-3">
+        <p>
+          I’m a Front-End Developer passionate about creating seamless,
+          accessible, and pixel-perfect user interfaces that combine thoughtful
+          design with solid engineering principles.
+        </p>
+        <p>
+          I believe in the power of technology to create intuitive experiences,
+          and I strive to build digital products that are as functional as they
+          are beautiful.
+        </p>
+        <p>
+          Until January 2025, I worked as a Web Developer at&nbsp;
+          <a
+            href="https://www.optatumplatform.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" hover:text-blue-500 font-bold"
+          >
+            Optaum Platform
+          </a>
+          , a payment solutions company, where I focused on developing and
+          maintaining responsive, high-performance web applications. My role
+          involved ensuring that the user interfaces were not only visually
+          appealing but also optimized for both performance and accessibility.
+        </p>
+        <p>
+          Before my role at Optaum, I completed an intensive 6-month Front-End
+          Development course at&nbsp;
+          <a
+            href="https://busan.greenart.co.kr/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" hover:text-blue-500 font-bold"
+          >
+            Green Computer Academy
+          </a>
+          , where I gained hands-on experience in building modern, responsive
+          websites and applications. This foundation laid the groundwork for my
+          current work and fueled my passion for creating user-centric digital
+          experiences.
+        </p>
+        <p>
+          In my spare time, you can find me experimenting with new web
+          technologies, solving coding challenges, or enjoying outdoor
+          activities like hiking or playing games.
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
